184f337bbdac325a418281666e833d7711517326
jellymlg <erikpanta@gmail.com>
2025-01-26 05:55:17 +0100

    Add address view

---
+++ components/ui/address-view.tsx
@@ -0,0 +1,112 @@
+import { BalanceResponse, TxsResponse } from "@/pages/address";
+import { Separator } from "./separator";
+import { DataTable } from "./data-table";
+import { IndexedErgoTransaction } from "@/lib/ergo-api";
+import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+import { TxType } from "./tx-type";
+import { TxStatus } from "./tx-status";
+import { feeFromTx } from "@/lib/utils";
+
+interface AddressViewProps {
+  address: string;
+  txs: TxsResponse | undefined;
+  balance: BalanceResponse;
+}
+
+export const IndexedTxColumns: ColumnDef<IndexedErgoTransaction>[] = [
+  {
+    accessorKey: "timestamp",
+    header: "Timestamp",
+    cell: ({ row }) => {
+      return new Date(row.original.timestamp).toLocaleString();
+    },
+  },
+  {
+    accessorKey: "id",
+    header: "Transaction Hash",
+    cell: ({ row }) => {
+      return (
+        <Link
+          className="text-primary underline"
+          href={"tx?id=" + row.original.id}
+        >
+          {row.original.id}
+        </Link>
+      );
+    },
+  },
+  {
+    id: "status",
+    header: "Status",
+    cell: ({}) => {
+      return (
+        <div className="flex justify-center">
+          <TxStatus confirmed={true} />
+        </div>
+      );
+    },
+  },
+  {
+    id: "type",
+    header: "Type",
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-center">
+          <TxType inputs={row.original.inputs} outputs={row.original.outputs} />
+        </div>
+      );
+    },
+  },
+  {
+    id: "fee",
+    header: "Fee",
+    cell: ({ row }) => {
+      return feeFromTx(row.original) / 1_000_000_000 + " ERG";
+    },
+  },
+];
+
+export function AddressView({ address, txs, balance }: AddressViewProps) {
+  return (
+    <div>
+      <div className="flex flex-wrap justify-center mx-auto rounded-lg border m-4 w-3/4 p-6 text-lg bg-black bg-opacity-70">
+        <div className="flex flex-wrap w-full">
+          <div className="w-1/4 flex content-center flex-wrap">Address:</div>
+          <div className="w-3/4 truncate">{address}</div>
+        </div>
+        <Separator className="m-3" />
+        <div className="flex flex-wrap w-full">
+          <div className="w-1/4 flex content-center flex-wrap">
+            Transactions:
+          </div>
+          <div className="w-3/4 truncate">{txs?.total}</div>
+        </div>
+        <Separator className="m-3" />
+        <div className="flex flex-wrap w-full">
+          <div className="w-1/4 flex content-center flex-wrap">
+            Confirmed balance:
+          </div>
+          <div className="w-3/4 truncate">
+            {(balance.confirmed?.nanoErgs as number) / 1_000_000_000 + " ERG"}
+          </div>
+        </div>
+        <Separator className="m-3" />
+        <div className="flex flex-wrap w-full">
+          <div className="w-1/4 flex content-center flex-wrap">
+            Unonfirmed balance:
+          </div>
+          <div className="w-3/4 truncate">
+            {(balance.unconfirmed?.nanoErgs as number) / 1_000_000_000 + " ERG"}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        <DataTable
+          columns={IndexedTxColumns}
+          data={txs?.items as IndexedErgoTransaction[]}
+        />
+      </div>
+    </div>
+  );
+}
--- pages/address.tsx
+++ pages/address.tsx
@@ -1,34 +1,39 @@
-import { ErgoApi, IndexedErgoTransaction } from "@/lib/ergo-api";
+import { AddressView } from "@/components/ui/address-view";
+import { BalanceInfo, ErgoApi, IndexedErgoTransaction } from "@/lib/ergo-api";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+export interface TxsResponse {
+  items?: IndexedErgoTransaction[];
+  total?: number;
+}
+
+export interface BalanceResponse {
+  confirmed?: BalanceInfo;
+  unconfirmed?: BalanceInfo;
+}
+
 export default function Address() {
   const addr: string = useSearchParams().get("id") as string;
-  const [address, setAddress] = useState<
-    | {
-        items?: IndexedErgoTransaction[];
-        total?: number;
-      }
-    | undefined
-  >();
+  const [txs, setTxs] = useState<TxsResponse>();
+  const [balance, setBalance] = useState<BalanceResponse>();
   useEffect(() => {
     if (!addr) return;
     const api = new ErgoApi();
     api.baseUrl = "http://213.239.193.208:9053";
     const fun = async () => {
-      api.blockchain
-        .getTxsByAddress(addr, { offset: 0, limit: 30 })
-        .then((resp) => setAddress(resp.data));
+      api.blockchain.getAddressBalanceTotal(addr).then((resp) => {
+        setBalance(resp.data);
+        api.blockchain
+          .getTxsByAddress(addr, { offset: 0, limit: 30 })
+          .then((resp) => setTxs(resp.data));
+      });
     };
     fun();
-  }, [addr]);
-  if (!address) {
+  }, [balance, addr]);
+  if (!balance || !txs) {
     return <p>Not found address {addr}</p>;
   } else {
-    return (
-      <div>
-        <p>Found address {addr}</p>
-      </div>
-    );
+    return <AddressView address={addr} txs={txs} balance={balance} />;
   }
 }
