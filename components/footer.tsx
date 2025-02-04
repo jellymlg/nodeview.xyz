c0fe4fc9ceb06d38a5c213ea7bc87519170d2f7a
jellymlg <erikpanta@gmail.com>
2025-02-04 23:38:36 +0100

    Reformat

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,10 +7,7 @@ export function Footer() {
   return (
     <footer className="flex items-center justify-between min-w-max min-h-max border rounded-lg relative clear-both mt-4 p-4">
       <ModeToggle />
-      <Link
-        href="https://github.com/jellymlg/nodeview.xyz"
-        target="_blank"
-      >
+      <Link href="https://github.com/jellymlg/nodeview.xyz" target="_blank">
         <GithubIcon />
       </Link>
       <NodeChangeDialog />
